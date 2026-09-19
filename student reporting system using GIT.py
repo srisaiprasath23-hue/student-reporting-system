8dd0d2834477ecf139274a65bcc2004874f2e239
srisaiprasath23-hue <srisaiprasath23@gmail.com>
2026-09-19 23:15:04 +0530

    last

--- student reporting system using GIT.py
+++ student reporting system using GIT.py
@@ -65,12 +65,4 @@ print("enn  peru prakash")
 
 print("otha na tha")
 print("iam hero")
-<<<<<<< HEAD
-  print("ma sai")
-=======
-
-
-print("hii")
-
-print("bye")
->>>>>>> 3c7f01f0ccda9da0285317c0207f6905621c06e7
+ 
